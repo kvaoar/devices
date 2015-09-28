a43c3aa37b5fb2d699d7135dd0014057111a02b4
kvaoar <kvrrus@gmail.com>
2015-09-28 03:22:54 +0300

    ESP8266 AP mode

--- micropirani/init.lua
+++ micropirani/init.lua
@@ -1,30 +1,15 @@
-local SSID = "***"
-local SSID_PASSWORD = "***"
-local DISABLED_CHARSET = "[\r\n\",:]";
+local DISABLED_CHARSET = "[\r\n\",:]"
 local CMD = nil
-local ANSWER = nil
-
--- udp server
+local ANSWER = "" 
+-- udp server 
 local function server_udp(client,data)
-CMD = string.gsub(data, DISABLED_CHARSET, "");  
-ANSWER = nil;
-uart.write(0, CMD.."\n")
-cnt = 1
-tmr.alarm (1, 300, 1, function ( )
-    cnt = cnt + 1   
-      if (ANSWER ~= nil) then
-         tmr.stop (1)
-         client:send ( "{\"CMD\": \""..CMD.."\", \"ANSW\": \""..ANSWER.."\"}" )
-         ANSWER = nil;
-      end
-      if(cnt > 5) then 
-          cnt = 0
-          tmr.stop (1)
-          client:send ( "{\"CMD\": \""..CMD.."\",\"ANSW\": \"".."TIMEOUT".."\"}" )
-          ANSWER = nil;
-      end
-   end)
+    CMD = string.gsub(data, DISABLED_CHARSET, "")
+    uart.write(0, CMD.."\n")
+    tmr.delay(1000) 
+    client:send ( ANSWER )
+    ANSWER = ""
 end
+
 -- connect to wifi
 function wait_for_wifi_conn ( )
    tmr.alarm (2, 1000, 1, function ( )
@@ -33,30 +18,36 @@ function wait_for_wifi_conn ( )
          print(wifi.sta.getip())
       end
    end)
-end
--- connect to usart
+end  
+
+--init uart
 function usart_init()
 gpio14 = 5;
 gpio.mode(gpio14, gpio.INPUT, gpio.PULLUP)
 if gpio.read(gpio14) == 1 then 
     uart.setup( 0, 9600, 8, 0, 1, 0 )
     print("uart on")
-    uart.on("data", "\n", 
+    uart.on("data", 0, 
     function(data)
-        ANSWER = string.gsub(data, DISABLED_CHARSET, "")
-        print(ANSWER)
+    ANSWER = ANSWER..string.gsub(data, DISABLED_CHARSET, "")
     end, 0)
 end
 end
--- Configure the ESP as station (client)
-wifi.setmode (wifi.STATION)
-wifi.sta.config (SSID, SSID_PASSWORD)
-wifi.sta.autoconnect (1)
--- if we want change script - we cant use uart
+
+local str=wifi.ap.getmac()
+local ssidTemp=string.format("%s%s%s",string.sub(str,10,11),string.sub(str,13,14),string.sub(str,16,17))
+cfg={}
+cfg.ssid="ESP8266_"..ssidTemp
+cfg.pwd="12345678"
+wifi.ap.config(cfg)
+cfg={}
+cfg.ip="192.168.1.1"
+cfg.netmask="255.255.255.0"
+cfg.gateway="192.168.1.1"
+wifi.ap.setip(cfg)
+wifi.setmode(wifi.SOFTAP)
+collectgarbage();
 usart_init()
--- Hang out until we get a wifi connection before the httpd server is started.
-wait_for_wifi_conn ()
--- Create the udp server
 svr = net.createServer (net.UDP)
 svr:on("receive", server_udp)
 svr:listen (8888)
